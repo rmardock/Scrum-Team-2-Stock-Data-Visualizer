6e0b132b72fb69663796b3d061723ff3124b5c96
Ryan <rsmkr8@protonmail.ch>
2022-10-20 14:12:12 -0500

    Added function to get data from API
    This function gets data from the API and returns it as a JSON Object. Additional details in the code comments.

--- stock_data_visualizer.py
+++ stock_data_visualizer.py
@@ -1,1 +1,23 @@
+import json
+import requests
 
+# Function to query API
+# functionType and sybmol will be provided by user input from their respective functions
+# I tested this function using test values and it does work 
+# It will return ALL information from the last 20 years in JSON format 
+# We can build a function to parse the information into a usable format after completing other modules
+
+# Global variables for output size and API key
+outputSize = "full"
+key = "DLEZPCELNFARX2UF"
+
+# Function to get data from the API
+def queryAPI(functionType, symbol, outputSize, key):
+    # URL construction for API request
+    url = "https://www.alphavantage.co/query?function=" + functionType + "&symbol=" + symbol + "&outputsize=" + outputSize + "&apikey=" + key
+    # Query API
+    response = requests.request("GET", url)
+    # Format data as JSON object
+    data = response.json()
+    # Return data
+    return data

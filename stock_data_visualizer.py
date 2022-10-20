c2b1c3235816834440c18b716c8b964c43bd4919
Ryan <rsmkr8@protonmail.ch>
2022-10-20 14:20:38 -0500

    Updated global variables
    I realized we should only need to use the TIME_SERIES_DAILY function in our API calls. I updated the global variables to reflect this.

--- stock_data_visualizer.py
+++ stock_data_visualizer.py
@@ -2,12 +2,13 @@ import json
 import requests
 
 # Function to query API
-# functionType and sybmol will be provided by user input from their respective functions
+# Sybmol will be provided by user input from its respective function
 # I tested this function using test values and it does work 
 # It will return ALL information from the last 20 years in JSON format 
 # We can build a function to parse the information into a usable format after completing other modules
 
-# Global variables for output size and API key
+# Global variables for function, output size, and API key parameters
+functionType = "TIME_SERIES_DAILY"
 outputSize = "full"
 key = "DLEZPCELNFARX2UF"
 

fd56cbcd8519fededcab9868ab4ecae7345bac0c
austinjyoung <89602020+austinjyoung@users.noreply.github.com>
2022-10-24 15:04:44 -0500

    Update stock_data_visualizer.py

--- stock_data_visualizer.py
+++ stock_data_visualizer.py
@@ -1,15 +1,24 @@
 import json
 import requests
 import pygal
+import datetime
 
 # Global variable for while loop
 run = True
-    
+
+
 # Function to to supply prompt and get user input
 def userInput(prompt, inputText):
     # Prompt user
     print(prompt)
     # Get user input
+    selection = input(inputText)
+    # Return user input
+    return selection
+
+# Function to get user input for dates
+def userInputDates(inputText):
+    # Get user input from dates function
     selection = input(inputText)
     # Return user input
     return selection
@@ -23,33 +32,80 @@ def chartSelection(invalidInputText):
     # Call userInput function
     selection = userInput(prompt, inputText)
     # If user selection is not a given option, prompt user to try again
-    while(selection != "1" and selection != "2"):
+    while (selection != "1" and selection != "2"):
         selection = userInput("", invalidInputText)
-    if(selection == "1"):
+    if (selection == "1"):
         return "line"
-    elif(selection == "2"):
+    elif (selection == "2"):
         return "bar"
-    
+
+
 # Function to get user input for time series selection
 def timeSeries(invalidInputText):
     # User input prompt variable
     prompt = "Select the Time Series of the chart you wan to generate:\n1. Intraday\n2. Daily\n3. Weekly\n4. Monthly\n"
-    # Input Text variable 
+    # Input Text variable
     inputText = "Enter the time series option (1, 2, 3, 4): "
     selection = userInput(prompt, inputText)
     # If user selection is not a given option, prompt user to try again
-    while(selection != "1" and selection != "2" and selection != "3" and selection != "4"):
+    while (selection != "1" and selection != "2" and selection != "3" and selection != "4"):
         selection = userInput("", invalidInputText)
-    if(selection == "1"):
+    if (selection == "1"):
         return "TIME_SERIES_INTRADAY"
-    if(selection == "2"):
+    if (selection == "2"):
         return "TIME_SERIES_DAILY"
-    if(selection == "3"):
+    if (selection == "3"):
         return "TIME_SERIES_WEEKLY"
-    if(selection == "4"):
+    if (selection == "4"):
         return "TIME_SERIES_MONTHLY"
 
-# Function to parse data 
+# Function to get user input for start date
+def startDate():
+    date_format = '%Y-%m-%d'
+    inputText = "Enter the start Date (YYYY-MM-DD): "
+    selection = userInputDates(inputText)
+    # using try-except blocks for handling the exceptions
+    while True:
+        try:
+            # formatting the date using strptime() function
+            dateObject = datetime.datetime.strptime(selection, date_format)
+            # formatting the dateObject to exclude time
+            dateObjectWithoutTime = dateObject.strftime(date_format)
+            print(dateObjectWithoutTime)
+            # If the date validation goes wrong
+        except ValueError:
+            # printing the appropriate text if ValueError occurs
+            print("Incorrect date format. Enter the start Date in YYYY-MM-DD format: ")
+            selection = userInputDates(inputText)
+        else:
+            #No error, break loop
+            break
+    return dateObjectWithoutTime
+
+# Function to get user input for end date
+def endDate():
+    date_format = '%Y-%m-%d'
+    inputText = "Enter the end Date (YYYY-MM-DD): "
+    selection = userInputDates(inputText)
+    # using try-except blocks for handling the exceptions
+    while True:
+        try:
+            # formatting the date using strptime() function
+            dateObject = datetime.datetime.strptime(selection, date_format)
+            # formatting the dateObject to exclude time
+            dateObjectWithoutTime = dateObject.strftime(date_format)
+            print(dateObjectWithoutTime)
+            # If the date validation goes wrong
+        except ValueError:
+            # printing the appropriate text if ValueError occurs
+            print("Incorrect date format. Enter the end Date in YYYY-MM-DD format: ")
+            selection = userInputDates(inputText)
+        else:
+            # No error, break loop
+            break
+    return dateObjectWithoutTime
+
+# Function to parse data
 # We will need to replace the second column ["2022-10-20"] with a variable stemming from date selection functions
 def parseData(data, timeSeries):
     open = data[timeSeries]["2022-10-20"]["1. open"]
@@ -57,17 +113,19 @@ def parseData(data, timeSeries):
     low = data[timeSeries]["2022-10-20"]["3. low"]
     close = data[timeSeries]["2022-10-20"]["4. close"]
     return open, high, low, close
-    
+
+
 # Function to assign time series for JSON
 def jsonTime(timeOption):
-    if(timeOption == "TIME_SERIES_INTRADAY"):
+    if (timeOption == "TIME_SERIES_INTRADAY"):
         return "Time Series (5min)"
-    elif(timeOption == "TIME_SERIES_DAILY"):
+    elif (timeOption == "TIME_SERIES_DAILY"):
         return "Time Series (Daily)"
-    elif(timeOption == "TIME_SERIES_WEEKLY"):
+    elif (timeOption == "TIME_SERIES_WEEKLY"):
         return "Weekly Time Series"
-    elif(timeOption == "TIME_SERIES_MONTHLY"):
+    elif (timeOption == "TIME_SERIES_MONTHLY"):
         return "Monthly Time Series"
+
 
 # Function to index data into dictionary
 def indexData(data, timeSeries):
@@ -90,14 +148,15 @@ def indexData(data, timeSeries):
         i += 1
     return graphData
 
-# Function to build chart 
+
+# Function to build chart
 def buildChart(graphData, chartType, data, timeSeries):
     # Variable for iterating dictionary
     i = 0
     # List for adding values to graphs
     list = []
     # If line chart is selected
-    if(chartType == "line"):
+    if (chartType == "line"):
         # Create line chart
         lineChart = pygal.Line()
         # For all data entries, iterate and add to list
@@ -121,9 +180,10 @@ def buildChart(graphData, chartType, data, timeSeries):
         # Render graph in browser
         barChart.render_in_browser()
     # Error handling
-    elif(chartType != "line" and chartType != "bar"):
+    elif (chartType != "line" and chartType != "bar"):
         # Error message
         print("ERROR")
+
 
 # Function to get data from the API
 def queryAPI(functionType, symbol, outputSize, key):
@@ -134,13 +194,14 @@ def queryAPI(functionType, symbol, outputSize, key):
     # Format data as JSON object
     data = response.json()
     # Return data
-    return data    
+    return data
+
 
 # While loop to run program and allow for repetition
-while(run == True):
+while (run == True):
     # Variable for invalid selection input text
     invalidInputText = "Invalid option! Enter the number of the option you want to select: "
-    
+    invalidInputDatesFormat = "Invalid date! Enter the date in YYYY-MM-DD format:  "
     # Variables for output size and API key parameters
     outputSize = "full"
     key = "DLEZPCELNFARX2UF"
@@ -151,16 +212,29 @@ while(run == True):
     # Ask the user for the stock symbol they would like to visualize
     user_symbol = input("Enter the stock symbol you are looking for: ")
     print()
-    
+
     # Call function for chart selection and assign value ("line" or "bar")
     # This value will be used to open the correct chart in the browser
     chartOption = chartSelection(invalidInputText)
-    
+
     # Call function for time series selection and assign value
     functionType = timeSeries(invalidInputText)
+
+    # Call function for start date
+    startTime = startDate()
+
+    # Call function for end date
+    endTime = endDate()
+
+    # Checks to see if start time value is after endtime and resets if true
+    while (startTime >= endTime):
+        print("Start date cannot be later than or equal to End date. Enter the dates again.")
+        startTime = startDate()
+        endTime = endDate()
+
     # Assign correct format for JSON based on user selection
     jTime = jsonTime(functionType)
-    
+
     # Pass the symbol to the queryAPI function
     data = queryAPI(functionType, user_symbol, outputSize, key)
     # Call function to parse and index data for graph
@@ -171,7 +245,7 @@ while(run == True):
     #buildChart(graphData, chartOption, data, jTime)
     # Check if the user would like to visualize another stock
     user_continue = input("Would you like to view more stock data? Press 'y' to continue: ")
-    if(user_continue == "y"):
+    if (user_continue == "y"):
         # Repeat program by iterating through while loop again
         run = True
         # Empty dictionary
